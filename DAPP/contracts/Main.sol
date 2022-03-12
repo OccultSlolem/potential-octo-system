f64327ddce39f21b89772a104ab1a3f7f413f1fd
Aidan <abayercalvert@gmail.com>
2022-03-12 15:11:17 -0800

    l

--- DAPP/contracts/Main.sol
+++ DAPP/contracts/Main.sol
@@ -6,20 +6,46 @@ pragma solidity ^0.8.11;
 
 
 contract Main {
+    // ---------------------------------------- data
+    string public merchantName;
+    string public productName;
+    uint256 public cost;
+    uint256 public timeFrame;
+    uint256 public merchantAddress;
+   
+   constructor (string merchantName_, string productName_, uint256 cost_, uint256 timeFrame_, uint256 merchantAddress)){
+    merchantName = merchantName_;
+    productName = productName_;
+    cost = cost_;
+    timeFrame = timeFrame_;
+    merchantAddress = merchantAddress_;
+   }
+   
+    mapping(address => uint) public userBalance;
+   // ------------------------------------------ paying/main 
+    //function that is in charge of paying to address
+    function transfer(address sender_, address merchantAddress_, uint256 amount_) {
+        if(sender_ < cost) {
 
-        string merchantName;
-        string productName;
-        uint cost;
-        uint timeFrame;
-        uint address;
-
-    constructor (string _merchantName, string _productName, uint _cost, uint timeFrame, address _address) public {
-        merchantName = _merchantName;
-        productName = _productName;
-        Cost
+        }
     }
-
-    function hello () public pure returns (string memory ) {
-        return "Hello World";
+        
+   
+    //function that checks balance
+    function balanceOf(address account) external view returns(uint){
+        return address(this).balance;
     }
+    //checks if subscription is active
+
+
+
+    // ------------------------------------ subscription stuff
+    bool isSubscriptionActive = false;
+
+
+
+    
+
+
+
 }
