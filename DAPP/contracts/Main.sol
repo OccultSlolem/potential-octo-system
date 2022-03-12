b9a0c979a18f641a983d1094715a2199d5392e2a
Aidan <abayercalvert@gmail.com>
2022-03-11 22:21:45 -0800

    constructor fun

--- DAPP/contracts/Main.sol
+++ DAPP/contracts/Main.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.8.11;
 
 //main contract 
 
+
+
 contract Main {
+
+        string merchantName;
+        string productName;
+        uint cost;
+        uint timeFrame;
+        uint address;
+
+    constructor (string _merchantName, string _productName, uint _cost, uint timeFrame, address _address) public {
+        merchantName = _merchantName;
+        productName = _productName;
+        Cost
+    }
 
     function hello () public pure returns (string memory ) {
         return "Hello World";
