d013a43bb5e6c68ed8d772b6860152329bd33038
Aidan <abayercalvert@gmail.com>
2022-03-12 15:48:11 -0800

    d

--- DAPP/contracts/Main.sol
+++ DAPP/contracts/Main.sol
@@ -13,33 +13,38 @@ contract Main {
     uint256 public timeFrame;
     uint256 public merchantAddress;
    
-   constructor (string merchantName_, string productName_, uint256 cost_, uint256 timeFrame_, uint256 merchantAddress)){
+   constructor (string memory merchantName_, string memory productName_, uint256 cost_, uint256 timeFrame_, uint256 merchantAddress_){
     merchantName = merchantName_;
     productName = productName_;
     cost = cost_;
     timeFrame = timeFrame_;
     merchantAddress = merchantAddress_;
    }
-   
-    mapping(address => uint) public userBalance;
+    //finds users address 
+    address public userAddress = msg.sender;
+    mapping(userAddress =>)
+
+
    // ------------------------------------------ paying/main 
     //function that is in charge of paying to address
-    function transfer(address sender_, address merchantAddress_, uint256 amount_) {
+    function transfer (address sender_, address merchantAddress_, uint amount_) public {
         if(sender_ < cost) {
-
+            
         }
     }
         
    
     //function that checks balance
-    function balanceOf(address account) external view returns(uint){
+    function balanceOf(address account) external view returns(uint) {
         return address(this).balance;
     }
-    //checks if subscription is active
+
 
 
 
     // ------------------------------------ subscription stuff
+
+    
     bool isSubscriptionActive = false;
 
     //cancel subscription
