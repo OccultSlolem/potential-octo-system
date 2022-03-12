ae7c38203b3ba888a955d69e430c680ed64e8e57
Ethan Hanlon <ehanlon19@gmail.com>
2022-03-11 20:04:49 -0800

    Added JSON middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+app.use(express.json());
 
 app.listen(
     PORT,
@@ -10,4 +13,17 @@ app.get('/hello', (req, res) => {
     res.status(200).send({
         message: 'Hello, world!'
     })
+})
+
+app.post('/hello/:id', (req,res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo! '})
+    };
+
+    res.status(200).send({
+        tshirt: '👕 with your ' + logo + 'and ID of ' + id
+    }); 
 })
