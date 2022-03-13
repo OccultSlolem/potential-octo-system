34077605b053bf7f8a1a7b7eadfd7577db198115
mar10fam <mphamrtin@gmail.com>
2022-03-12 20:13:48 -0800

    purchase page

--- trilinkt/src/routes/purchase/index.svelte
+++ trilinkt/src/routes/purchase/index.svelte
@@ -2,6 +2,7 @@
 import { onMount } from "svelte";
 import './index.css'
 import removenavbar from "../__layout.svelte"
+import { stringify } from "postcss";
     onMount(() => {
         removenavbar()
     })
@@ -14,12 +15,18 @@ import removenavbar from "../__layout.svelte"
     //datatypes for product info
     let productname = "product";
     let price = .001;
-    let period = -1;
+    let period = 30;
     let id = 1234;
+    let description = "product description";
+
+    //logic for calculating the subscription
+    let subscription = "";
+    if(period == -1) subscription = "one time purchase"
+    else subscription = `${price}ETH / ${period} days`
 
 </script>
 
-<div>
+<!-- <div>
     <h1 class="header">trilinkt</h1>
     <div class="center"> 
         <div>
@@ -28,8 +35,35 @@ import removenavbar from "../__layout.svelte"
         <h3 class="lining">Merchant Name: {merchantname}</h3>
         
     </div>
+</div> -->
+
+<div class="main">
+    <h1 class="header">trilinkt</h1>
+    <div class="center">
+        <div class="overflow-x-auto">
+        <table class="table table-normal w-full">
+            <!-- table head-->
+            <thead>
+                <tr>
+                    <th class="left-th">Merchant</th>
+                    <th class="center-th">Product</th>
+                    <th class="right-th">Subscription Charge</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- table row 1 -->
+                <tr>
+                    <td class="left-th">{merchantname}</td>
+                    <td class="center-th">{productname}</td>
+                    <td class="right-th">{subscription}</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+     <h3 class="price">Due now: {price}ETH</h3>
+     <h1><b>Description</b></h1>
+     <br>
+     <p>{description}</p>
+     
 </div>
-
-
-
-   
