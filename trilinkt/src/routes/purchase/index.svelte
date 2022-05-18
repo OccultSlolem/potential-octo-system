aa4512e266231d9124752c4a822a015a3bfce0e5
Ethan Hanlon <me@ethan-hanlon.xyz>
2022-05-18 10:56:04 -0700

    changes 5/18/22

--- trilinkt/src/routes/purchase/index.svelte
+++ trilinkt/src/routes/purchase/index.svelte
@@ -1,83 +1,49 @@
-<script land="ts">
-import { onMount } from "svelte";
-import './index.css';
-import '/Users/aidan/Desktop/Code/hackathonSFSU2022/potential-octo-system/DAPP/contracts/Main.sol';
-import '/Users/aidan/Desktop/Code/hackathonSFSU2022/potential-octo-system/trilinkt/src/routes/purchase/payment.svelte';
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { collection, doc, getDocs, query, where, type DocumentData } from 'firebase/firestore';
+	import { firestore } from '../../Firebase';
 
+	// let period: number;
+	// let price: number;
+    let data: DocumentData;
+    let prodName = "Coding Cavaliers+";
+    let prodPrice = 0.01;
+    let prodPeriod = 30;
 
+	async function load() {
+		let slug = $page.params.slug;
+		let apiKeyIndex = slug.indexOf('apikey');
+		let endApiIndex = slug.indexOf('_', apiKeyIndex + 7);
+        let productIndex = slug.indexOf('product');
+        let endProductIndex = slug.indexOf('_', productIndex + 8)
+        console.log(endApiIndex)
+        
 
-import removenavbar from "../__layout.svelte"
-    onMount(() => {
-        // removenavbar()
-    })
-    
-    //create variables for everything the merchant would want to input to setup the purchase page that WE will generate
-    let apikey = "myapikey";
-    let merchantname = "Merchant";
-    let logo = "https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo-700x394.png";
-
-    //datatypes for product info
-    let productname = "product";
-    let price = .001;
-    let period = 30;
-    let id = 1234;
-    let description = "product description";
-
-    //logic for calculating the subscription
-    let subscription = "";
-    if(period == -1) subscription = "one time purchase"
-    else subscription = `${price}ETH / ${period} days`
-    
-    
-    //timer 
-
-    // const timePeriod = addEventListener(#timerinput);
-    // const cancelSub = addEventListener(#cancelsub);
-    let timeCounter = 0;
-    let subscriptionStatus = false;
-
-   // call this the first time paying 
-    function changeSubStatus () {
-        subscriptionStatus == true;
-    }
-
-    //counting down by days
-    //when asking for user input on time frame for sub
-    //make sure to take the value as days
-    // setInterval(subDay(), 1000 * 60 * 60 * 24); 
- 
-    //resets counter time, makes it reset to timeperiod
-    function resetCounter() {
-        // timeCounter = timeCounter + timePeriod;
-    }
-    //this runs after a day
-    function subDay() {
-        timeCounter = timeCounter - 1;
-        // setInterval()
-    }
-
-    // if(subscriptionStatus == true) {
-    //     if (timeCounter = 0) {
-    //         resetCounter()
-    //         transferFunds(userAddress, merchantAddress, cost)
-    //   }
-      
-    //   else {
-    //       return "error"
-    //   }
-    // }   
-
-    // if(cancelSub == true) {
-    //     isSubscriptionActive == false;
-
-    // }
-
-
-
-
-
-
+		if (endApiIndex != -1 && endProductIndex != -1) {
+            console.log(2);
+			let apikey = slug.substring(apiKeyIndex + 7, endApiIndex);
+            let productKey = slug.substring(productIndex + 8, endProductIndex);
+			const q = query(collection(firestore, 'merchants'), where('apikey', '==', apikey));
+			await getDocs(q)
+			.then((querySnapshot) => {
+                console.log(3);
+			    querySnapshot.forEach((result) => {
+                    console.log(4);
+                    data = result.data();
+                    prodName  = data['products'][productKey].name;
+                    prodPrice = data['products'][productKey].price;
+                    prodPeriod = data['products'][productKey].period;
+                })
+			})
+			.catch((reason) => {
+			    console.error(reason);
+			});
+		}
+	}
 </script>
+
+<!-- {#if data != undefined} -->
+<!-- <h1>{data.name}</h1> -->
 
 <div class="main">
     <h1 class="header">trilinkt</h1>
@@ -95,22 +61,22 @@ import removenavbar from "../__layout.svelte"
             <tbody>
                 <!-- table row 1 -->
                 <tr>
-                    <td class="left-th">{merchantname}</td>
-                    <td class="center-th">{productname}</td>
-                    <td class="right-th">{subscription}</td>
+                    <td class="left-th">Coding Cavaliers</td>
+                    <td class="center-th">{prodName}</td>
+                    <td class="right-th">{prodPrice}</td>
                 </tr>
             </tbody>
         </table>
         </div>
     </div>
-     <h3 class="price">Due now: {price}ETH</h3>
+     <h3 class="price">Due now: {prodPrice}ETH</h3>
      <h1><b>Description</b></h1>
      <br>
-     <p>{description}</p>
-     
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, laboriosam blanditiis rerum beatae delectus dignissimos temporibus saepe, voluptates perferendis omnis atque expedita aut tempore enim quas corrupti culpa neque magnam.</p>
+     <div class="divider"></div>
+     <button class="btn btn-primary">Purchase</button>
+     <button class="btn btn-accent">Back to Coding Cavaliers</button>
 </div>
-
-
-
-
-
+<!-- {:else} -->
+<!-- <button on:click={load} class="btn btn-primary">Reload</button> -->
+<!-- {/if} -->
---
+++ trilinkt/start-emu.sh
@@ -0,0 +1,1 @@
+firebase emulators:start --import=./emulators-export --export-on-exit=./emulators-export
